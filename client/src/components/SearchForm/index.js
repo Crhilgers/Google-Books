73c2c1e041578c9803719fcddc25166bb14e71a4
crhilgers <cole5882@gmail.com>
2019-04-18 20:02:11 -0600

    fixing redundancy

--- client/src/components/SearchForm/index.js
+++ client/src/components/SearchForm/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function SearchForm({ q, handleInputChange, handleFormSubmit }) {
   return (
-    <form className="form-inline" role="form">
+    <form className="form-inline">
       <div className="form-group mx-sm-3 mb-2">
         <label htmlFor="Title" className="sr-only">
           Search Book Title
